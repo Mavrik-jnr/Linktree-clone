19c529e4cded95066e2a4b379aa7d112ad5858d3
Mahmoud Oluwatobi <mahmoud4u12@gmail.com>
2022-10-23 10:43:25 -0700

    Add modular css to profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,18 @@
 import React from "react";
 import logo from "../images/gigachad.jpg";
-import "./styles/profile.module.css";
+import styles from "./styles/profile.module.css";
+import cameraIcon from "../images/camera-01.svg";
 function Profile() {
   return (
     <>
-      <a href="/" id="hi">
-        omo
-      </a>
-      <div className="profile">
-        <img id="profile__img" src={logo} alt="" />
+      <div className={styles.profile}>
+        <img
+          id="profile__img"
+          className={styles.profile__img}
+          src={logo}
+          alt="Profile"
+        />
+        <img src={cameraIcon} className={styles.cam} alt="" />
       </div>
     </>
   );
