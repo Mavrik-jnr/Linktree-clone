7722fb146714c3957911631a63a0702ef0d4127e
Mahmoud Oluwatobi <mahmoud4u12@gmail.com>
2022-10-23 21:53:28 -0700

    Add Responsive Icon

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -4,21 +4,19 @@ import share from "../images/Share.svg";
 import dot from "../images/Dots.svg";
 import shareLink from "../images/shareTool.svg";
 import shape from "../images/Shape.svg";
+import useWindowDimensions from "../useWindow";
 // import icon2 from "../images/share-06.svg";
 
 function Share({ type, device, parentStyle }) {
+  const { width } = useWindowDimensions();
   return (
     <div
       tabIndex={1}
-      type={window.innerWidth >= 768 && type}
+      type={width <= 760 && type}
       device={device}
       className={`${parentStyle} ${styles.icon}`}
     >
-      <img
-        src={window.innerWidth <= 768 ? dot : share}
-        className={styles.arrow}
-        alt=""
-      />
+      <img src={width <= 760 ? dot : share} className={styles.arrow} alt="" />
       <img className={styles.shareLink} src={shareLink} alt="" />
       <img src={shape} className={styles.cursor} alt="" />
     </div>
