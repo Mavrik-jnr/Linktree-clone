30ab03aa3f354eb5c7b7aa5fbbe595075ded873a
Mahmoud Oluwatobi <mahmoud4u12@gmail.com>
2022-10-23 19:43:05 -0700

    Add parentStyle prop to Icon component

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -6,9 +6,14 @@ import shareLink from "../images/shareTool.svg";
 import shape from "../images/Shape.svg";
 // import icon2 from "../images/share-06.svg";
 
-function Share({ type, device }) {
+function Share({ type, device, parentStyle }) {
   return (
-    <div tabIndex={1} type={type} device={device} className={styles.icon}>
+    <div
+      tabIndex={1}
+      type={type}
+      device={device}
+      className={`${parentStyle} ${styles.icon}`}
+    >
       <img src={type === "dot" ? dot : share} className={styles.arrow} alt="" />
       <img className={styles.shareLink} src={shareLink} alt="" />
       <img src={shape} className={styles.cursor} alt="" />
