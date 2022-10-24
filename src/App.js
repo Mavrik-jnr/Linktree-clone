51c27aec66f670083d63346f2f4682ccde620052
Mahmoud Oluwatobi <mahmoud4u12@gmail.com>
2022-10-23 17:12:36 -0700

    Add profile section markup

--- src/App.js
+++ src/App.js
@@ -6,9 +6,19 @@ import Icon from "./components/Icon";
 function App() {
   return (
     <div className={styles.App}>
+      <main>
+        <section className="profile__section">
+          <Profile />
+          <h4 id="twitter">@Maverick__jnr</h4>
+          <h4 id="slack" className={styles.slack}>
+            Toby
+          </h4>
+        </section>
+      </main>
+      <Profile />
       <h4>Annette Black</h4>
       <Link link={"https://google.com"} text={"Zuri Books"} />
-      <Profile />
+
       <Icon type="dot" />
     </div>
   );
