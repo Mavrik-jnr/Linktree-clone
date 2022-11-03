fec01a730e308af29f5723f645ee1221a8789ee7
Mahmoud Oluwatobi <mahmoud4u12@gmail.com>
2022-11-03 11:59:16 +0100

    feat: Add React-Router-Dom

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,12 @@
 import React from "react";
 import styles from "contact.module.css";
+import Input from "../components/Input";
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <form>
+      <Input />
+    </form>
+  );
 }
 
 export default Contact;
