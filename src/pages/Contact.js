90265957d4d596c4ed136e6496470c6bc63b18fd
Mahmoud Oluwatobi <mahmoud4u12@gmail.com>
2022-11-03 17:09:09 +0100

    feat: Add customized input components to form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,36 @@
 import React from "react";
-import styles from "contact.module.css";
+import styles from "./styles/contact.module.css";
 import Input from "../components/Input";
 
 function Contact() {
   return (
     <form className={styles.form}>
-      <Input />
+      <Input
+        id={"first_name"}
+        type={"First Name"}
+        parentClass={styles.first}
+        inputPlaceholder={"Enter your first name"}
+      />
+      <Input
+        id={"last_name"}
+        type={"Last Name"}
+        parentClass={styles.last}
+        inputPlaceholder={"Enter your last name"}
+      />
+      <Input
+        id={"email"}
+        type={"Email"}
+        parentClass={styles.email}
+        inputPlaceholder={"yourname@email.com"}
+      />
+      <Input
+        id={"message"}
+        type={"message"}
+        inputPlaceholder={
+          "Send me a message and i'll reply you as soon as possible..."
+        }
+      />
+      <Input id={"agreement"} type={"agreement"} />
     </form>
   );
 }
