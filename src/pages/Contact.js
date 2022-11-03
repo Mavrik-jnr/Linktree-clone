c605f35588cd366440781b5109b2bb4e0bdeb49b
Mahmoud Oluwatobi <mahmoud4u12@gmail.com>
2022-11-03 20:23:24 +0100

    feat: Add customized Input components

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,34 +4,40 @@ import Input from "../components/Input";
 
 function Contact() {
   return (
-    <form className={styles.form}>
-      <Input
-        id={"first_name"}
-        type={"First Name"}
-        parentClass={styles.first}
-        inputPlaceholder={"Enter your first name"}
-      />
-      <Input
-        id={"last_name"}
-        type={"Last Name"}
-        parentClass={styles.last}
-        inputPlaceholder={"Enter your last name"}
-      />
-      <Input
-        id={"email"}
-        type={"Email"}
-        parentClass={styles.email}
-        inputPlaceholder={"yourname@email.com"}
-      />
-      <Input
-        id={"message"}
-        type={"message"}
-        inputPlaceholder={
-          "Send me a message and i'll reply you as soon as possible..."
-        }
-      />
-      <Input id={"agreement"} type={"agreement"} />
-    </form>
+    <div className={styles.Contact}>
+      <div className={styles.headings}>
+        <h3>Contact Me</h3>
+        <h4>Hi there, contact me to ask me about anything you have in mind</h4>
+      </div>
+      <form className={styles.form}>
+        <Input
+          id={"first_name"}
+          type={"First Name"}
+          parentClass={styles.first}
+          inputPlaceholder={"Enter your first name"}
+        />
+        <Input
+          id={"last_name"}
+          type={"Last Name"}
+          parentClass={styles.last}
+          inputPlaceholder={"Enter your last name"}
+        />
+        <Input
+          id={"email"}
+          type={"Email"}
+          parentClass={styles.email}
+          inputPlaceholder={"yourname@email.com"}
+        />
+        <Input
+          id={"message"}
+          type={"message"}
+          inputPlaceholder={
+            "Send me a message and i'll reply you as soon as possible..."
+          }
+        />
+        <Input id={"agreement"} type={"agreement"} />
+      </form>
+    </div>
   );
 }
 
