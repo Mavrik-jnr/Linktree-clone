1e57f7a560f24a2702f23a5a98379f6c68b919e3
Mahmoud Oluwatobi <mahmoud4u12@gmail.com>
2022-11-04 13:31:44 +0100

    feat: Add Authentication to input fields

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,36 +2,76 @@ import React from "react";
 import styles from "./styles/contact.module.css";
 import Input from "../components/Input";
 import Button from "../components/Button";
+import { useState } from "react";
 
 function Contact() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [Email, setEmail] = useState("");
+  const [Message, setMessage] = useState("");
+  const [Error, setError] = useState(false);
+  const [completed, setCompleted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      firstName === "" ||
+      lastName === "" ||
+      !Email.toLowerCase().match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      ) ||
+      Message === ""
+    ) {
+      setError(true);
+    } else {
+      setError(false);
+      setFirstName("");
+      setEmail("");
+      setLastName("");
+      setMessage("");
+      setCompleted(true);
+    }
+  };
   return (
     <div className={styles.Contact}>
       <div className={styles.headings}>
         <h3>Contact Me</h3>
         <h4>Hi there, contact me to ask me about anything you have in mind</h4>
       </div>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
         <Input
           id={"first_name"}
+          Error={Error}
           type={"First Name"}
+          value={firstName}
+          onChange={setFirstName}
           parentClass={styles.first}
           inputPlaceholder={"Enter your first name"}
         />
         <Input
           id={"last_name"}
+          Error={Error}
           type={"Last Name"}
+          value={lastName}
+          onChange={setLastName}
           parentClass={styles.last}
           inputPlaceholder={"Enter your last name"}
         />
         <Input
           id={"email"}
           type={"Email"}
+          value={Email}
+          Error={Error}
+          onChange={setEmail}
           parentClass={styles.email}
           inputPlaceholder={"yourname@email.com"}
         />
         <Input
           id={"message"}
           type={"message"}
+          value={Message}
+          Error={Error}
+          onChange={setMessage}
           inputPlaceholder={
             "Send me a message and i'll reply you as soon as possible..."
           }
@@ -39,6 +79,7 @@ function Contact() {
         <Input id={"agreement"} type={"agreement"} />
         <Button />
       </form>
+      {completed && <p style={{ color: "green" }}>Message sent</p>}
     </div>
   );
 }
